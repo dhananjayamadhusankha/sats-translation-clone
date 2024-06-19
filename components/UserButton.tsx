d93f675ab964a932cb619f7dd8dc5faff8df79e3
dhananjayamadhusankha <mdhananjaya04@gmail.com>
2024-06-20 00:20:18 +0530

    properly set nextauth authentication for midleware

--- components/UserButton.tsx
+++ components/UserButton.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -7,12 +9,25 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import UserAvatar from "./UserAvatar";
+import { Session } from "next-auth";
+import { Button } from "./ui/button";
+import { signIn } from "next-auth/react";
 
-function UserButton() {
+function UserButton({ session }: { session: Session | null }) {
+  if (!session) {
+    return (
+      <Button variant={"outline"} onClick={() => signIn()}>
+        Sign In
+      </Button>
+    );
+  }
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
-        <UserAvatar name="Dhananjaya Madhusankha" image="https://github.com/shadcn.png" />
+        <UserAvatar
+          name="Dhananjaya Madhusankha"
+          image="https://github.com/shadcn.png"
+        />
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuLabel>My Account</DropdownMenuLabel>
--- middleware.ts
+++ middleware.ts
@@ -2,4 +2,4 @@ import { withAuth } from "next-auth/middleware";
 
 export default withAuth;
 
-export const config = { matcher: ["/chat", "/chat/:id/*", "/register"] };
+export const config = { matcher: ["/chat", "/chat/:id*", "/register"] };
