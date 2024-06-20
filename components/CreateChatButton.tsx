b5b2facdd2081231133b5d43b756756f2f390717
dhananjayamadhusankha <mdhananjaya04@gmail.com>
2024-06-20 08:12:58 +0530

    create chat button

---
+++ components/CreateChatButton.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { MessageSquarePlus } from "lucide-react";
+import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+
+function CreateChatButton() {
+  const route = useRouter();
+
+  const createNewChat = async () => {
+    // all logics here
+
+    route.push("/chat/${id}");
+  };
+  return (
+    <Button variant={"ghost"}>
+      <MessageSquarePlus />
+    </Button>
+  );
+}
+
+export default CreateChatButton;
--- components/Header.tsx
+++ components/Header.tsx
@@ -6,27 +6,31 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/auth";
 import Link from "next/link";
 import { MessagesSquare } from "lucide-react";
+import CreateChatButton from "./CreateChatButton";
 
 async function Header() {
   const session = await getServerSession(authOptions);
   console.log(session);
   return (
-    <header className='sticky top-0 z-50 bg-white dark:bg-gray-900'>
+    <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
       <nav className="flex flex-col sm:flex-row items-center p-5 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
         <Logo />
 
         <div className="flex flex-1 items-center space-x-4 justify-end">
           {/* LanguageSelect */}
+          
+          <>
+            {/* Session */}
+            {session ? (
+              <Link href={"/chat"} prefetch={false}>
+                <MessagesSquare />
+              </Link>
+            ) : (
+              <Link href={"/pricing"}>Price</Link>
+            )}
 
-          {/* Session */}
-          {session ? (
-            <Link href={"/chat"} prefetch={false}>
-              <MessagesSquare />
-            </Link>
-          ) : (
-            <Link href={"/pricing"}>Price</Link>
-          )}
-
+            <CreateChatButton />
+          </>
           {/* DarkModeToggle */}
           <ThemeToggler />
 
